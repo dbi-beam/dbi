8ce79e1f543db526f1edee27160fab4e4114c85c
Manuel Rubio <manuel@bosqueviejo.net>
2014-06-06 02:32:33 +0300

    typo error

--- src/dbi_utils.erl
+++ src/dbi_utils.erl
@@ -14,14 +14,14 @@ resolve(<<>>, SQL) ->
 resolve(<<"$$",Rest/binary>>, SQL) ->
     resolve(Rest, <<SQL/binary, "$">>);
 
-resolve(<<"$",A:1/binary,Rest/binary>>, SQL) when A >= $0 andalso A =< $9 ->
+resolve(<<"$",A:8,Rest/binary>>, SQL) when A >= $0 andalso A =< $9 ->
     resolve(drop_numbers(Rest), <<SQL/binary, "?">>);
 
 resolve(<<C:1/binary,Rest/binary>>, SQL) ->
     resolve(Rest, <<SQL/binary, C/binary>>).
 
 
-drop_numbers(<<A:1/binary,Rest/binary>>) when A >= $0 andalso A =< $9 ->
+drop_numbers(<<A:8,Rest/binary>>) when A >= $0 andalso A =< $9 ->
     drop_numbers(Rest);
 
 drop_numbers(Rest) ->
